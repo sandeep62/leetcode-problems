e7165cb91ee355d4745d51e24494227bd8b4596a
sandeep62 <109506135+sandeep62@users.noreply.github.com>
2024-04-21 23:04:56 +0530

    Time: 1076 ms (23.51%) | Memory: 234.1 MB (24.46%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,38 +1,32 @@
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
-    // Function to perform depth-first search to find a path from source to destination
-    bool solve(vector<vector<int>>& graph, int source, int destination, vector<int>& visited) {
-        // If source and destination are the same, a path is found
-        if (source == destination)
-            return true;
-        
-        // Mark the current node as visited
-        visited[source] = 1;
-        
-        // Traverse through all adjacent vertices of the current vertex
-        for (auto& neighbor : graph[source]) {
-            // If the neighbor is not visited, recursively search for a path
-            if (!visited[neighbor]) {
-                if (solve(graph, neighbor, destination, visited))
-                    return true; // If a path is found, return true
+    vector<vector<int>> adj;
+    vector<bool> viz;
+
+    bool dfs(int v, int t) {
+        if (v == t)
+            return 1;
+        viz[v] = 1;
+        for (int u : adj[v]) {
+            if (!viz[u]) {
+                if (dfs(u, t))
+                    return 1;
             }
         }
-        return false; // No path found from source to destination
+        return 0;
     }
-    
-    // Function to determine if a valid path exists from source to destination
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        // Create an adjacency list representation of the graph
-        vector<vector<int>> graph(n);
-        for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+
+    bool validPath(int n, vector<vector<int>>& edges, int source,
+                   int destination) {
+        adj.resize(n);
+        viz.assign(n, 0);
+        for (auto& e : edges) {
+            int u = e[0], v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        
-        // Initialize a vector to keep track of visited vertices
-        vector<int> visited(n, 0);
-        
-        // Call the helper function to find a path from source to destination
-        return solve(graph, source, destination, visited);
+
+        return dfs(source, destination);
     }
 };
