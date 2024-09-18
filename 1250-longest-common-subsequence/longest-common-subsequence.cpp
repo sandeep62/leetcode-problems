85313e63f0158381fa1b08f5a93fb1e0e3e803c0
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-09-18 23:45:24 +0530

    Time: 17 ms (85.68%) | Memory: 12.1 MB (85.75%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    //via recursion only there is time limit exceeded error so we try memoisation
-    int longestCommonSubsequence(string text1, string text2)
-    {
-        int n=text1.size();
-        int m=text2.size();
-        int t[n+1][m+1];
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0 ||j==0){
-                    t[i][j]=0;
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+        int dp[n+1][m+1];
+        // initialising the initial value of the tabolation table 
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
 
-                }
-            
-            }
+
         }
-        for(int i=1;i<=n;i++)
-        {
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++)
             {
-                if(text1[i-1]==text2[j-1])
-                {
-                    t[i][j]=1+t[i-1][j-1];
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
-                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
+
             }
         }
-        return t[n][m];
+        return dp[n][m];
+        
     }
 };
